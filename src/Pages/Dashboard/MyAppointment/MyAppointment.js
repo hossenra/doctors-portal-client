20e93b0819c24a43451fbbabd703ceef86c4ea21
RAZIBUL HOSSEN <razibulhossen@MacBook-Air-3.local>
2023-01-06 17:48:47 +0100

    payment data added

--- src/Pages/Dashboard/MyAppointment/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider';
 
 const MyAppointment = () => {
@@ -32,17 +33,32 @@ const MyAppointment = () => {
        <th>Treatment</th>
        <th>Date</th>
        <th>Time</th>
+       <th>Payment</th>
       </tr>
      </thead>
      <tbody>
       {
-       bookings && 
+       bookings &&
        bookings?.map((booking, i) => <tr key={booking._id}>
         <th>{i + 1}</th>
         <td>{booking.patient}</td>
         <td>{booking.treatment}</td>
         <td>{booking.appointmentDate}</td>
         <td>{booking.slot}</td>
+        <td>
+         {
+          booking.price && !booking.paid && <Link
+           to={`/dashboard/payment/${booking._id}`}
+          >
+           <button
+            className='btn btn-primary btn-sm'
+           >Pay</button>
+          </Link>
+         }
+         {
+          booking.price && booking.paid && <span className='text-green-500'>Paid</span>
+         }
+        </td>
        </tr>)
       }
      </tbody>
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -6,8 +6,10 @@ import AddDoctor from "../../Pages/Dashboard/AddDoctor/AddDoctor";
 import AllUsers from "../../Pages/Dashboard/AllUsers/AllUsers";
 import ManageDoctors from "../../Pages/Dashboard/ManageDoctors/ManageDoctors";
 import MyAppointment from "../../Pages/Dashboard/MyAppointment/MyAppointment";
+import Payment from "../../Pages/Dashboard/Payment/Payment";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
+import DisplayError from "../../Pages/Shared/DisplayError/DisplayError";
 import SignUp from "../../Pages/SignUp/SignUp";
 import AdminRoute from "../AdminRoute/AdminRoute";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
@@ -16,6 +18,7 @@ const router = createBrowserRouter([
  {
   path: '/',
   element: <Main></Main>,
+  errorElement: <DisplayError></DisplayError>,
   children: [
    {
     path: '/',
@@ -32,17 +35,18 @@ const router = createBrowserRouter([
    {
     path: '/appointment',
     element: <Appointment></Appointment>
-   },
-   {
-    path: '/appointment',
-    element: <Appointment></Appointment>
-   },
+   }
   ]
  },
  {
   path: '/dashboard',
   element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
+  errorElement: <DisplayError></DisplayError>,
   children: [
+   {
+    path: '/dashboard',
+    element: <MyAppointment></MyAppointment>
+   },
    {
     path: '/dashboard/allusers',
     element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
@@ -54,6 +58,11 @@ const router = createBrowserRouter([
    {
     path: '/dashboard/managedoctors',
     element: <AdminRoute><ManageDoctors></ManageDoctors></AdminRoute>
+   },
+   {
+    path: '/dashboard/payment/:id',
+    element: <AdminRoute><Payment></Payment></AdminRoute>,
+    loader: ({params}) => fetch(`http://localhost:5000/bookings/${params.id}`)
    },
   ]
  }
